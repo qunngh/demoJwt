445fb35e04d1ec0b929b53b474c8b97489156620
Minh Quang <hoquang1031@gmail.com>
2023-09-24 20:51:39 +0700

    Demo JWT

---
+++ src/main/java/com/demo/jwt/auth/AuthenticationService.java
@@ -0,0 +1,52 @@
+package com.demo.jwt.auth;
+
+import com.demo.jwt.config.JwtService;
+import com.demo.jwt.repository.UserRepository;
+import com.demo.jwt.user.Role;
+import com.demo.jwt.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    public AuthenticationRespond register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstname())
+                .lastName(request.getLastname())
+                .email(request.getEmail())
+                .pass(passwordEncoder.encode(request.getPassword()))
+                .role(Role.ADMIN)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationRespond.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationRespond authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+
+                )
+
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationRespond.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
